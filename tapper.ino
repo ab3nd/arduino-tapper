1e42d551cb5774e348db799bf4211c2729c75e70
ams <orphrey@gmail.com>
2021-05-30 21:46:59 -0400

    Apparently sparkfun's motorOff() didn't work ¯\_(ツ)_/¯ guess I'll do motorGo() with 0 speed

--- tapper.ino
+++ tapper.ino
@@ -94,16 +94,6 @@ void motorSetup()
   }
 }
 
-void motorOff(uint8_t motor)
-{
-  // Initialize brake to Vcc
-  for (int i = 0; i < 2; i++)
-  {
-    digitalWrite(inAPin[i], LOW);
-    digitalWrite(inBPin[i], LOW);
-  }
-  analogWrite(pwmPin[motor], 0);
-}
 
 /* motorGo() will set a motor going in a specific direction
   the motor will continue going in that direction, at that speed
@@ -154,7 +144,7 @@ void motorGo(uint8_t motor, uint8_t mode, uint8_t speed)
 
 void setup() {
   Serial.begin(9600);
-  Serial.print("Tapper Program is running");
+  //  Serial.print("Tapper Program is running");
   //pins 10 and 112 are LEDs to monitor the speed and duration of the motors
   pinMode(10, OUTPUT);
   pinMode(12, OUTPUT);
@@ -166,33 +156,31 @@ void setup() {
 }
 
 void loop() {
+
   bool pow_sw = digitalRead(3);
-  if (pow_sw) {
-    Serial.println("pow_sw is TRUE");
-  }
-  else {
-    Serial.println("pow_sw is FALSE");
-  }
+  //  if (pow_sw) {
+  //    Serial.println("pow_sw is TRUE");
+  //  }
+  //  else {
+  //    Serial.println("pow_sw is FALSE");
+  //  }
 
   //Speed - how fast the tappers alternate
   speedRotary = analogRead(4);
 
   //maps the speed from zero to 2 seconds
   mySpeed = map(speedRotary, 0, 1023, 0, 2000);
-  Serial.print("Current Speed: ");
-  Serial.println(mySpeed);
+  //  Serial.print("Current Speed: ");
+  //  Serial.println(mySpeed);
 
   //Duration - how long the tappers are active for
   durationRotary = analogRead(5);
 
   //maps the duration from zero to 2 seconds
   myDuration = map(durationRotary, 0, 1023, 0, 2000);
-  Serial.print("Current Duration: ");
-  Serial.println(myDuration);
-
-  Serial.print("State: ");
-  Serial.println(state);
-
+  //  Serial.print("Current Duration: ");
+  //  Serial.println(myDuration);
+  
   switch (state) {
     case OFF:
       if (pow_sw) {
@@ -203,8 +191,8 @@ void loop() {
       {
         digitalWrite(10, LOW);
         digitalWrite(12, LOW);
-        motorOff(MOTOR_A);
-        motorOff(MOTOR_B);
+        motorGo(MOTOR_A, CW, 0);
+        motorGo(MOTOR_B, CW, 0);
       }
       break;
 
@@ -222,8 +210,8 @@ void loop() {
       {
         digitalWrite(10, LOW);
         digitalWrite(12, HIGH);
-        motorOff(MOTOR_A);
-        motorGo(MOTOR_B, CW, PWM_MAX);
+        motorGo(MOTOR_A, CW, PWM_MAX);
+        motorGo(MOTOR_B, CW, 0);
       }
       break;
 
@@ -241,8 +229,8 @@ void loop() {
       {
         digitalWrite(10, HIGH);
         digitalWrite(12, LOW);
-        motorGo(MOTOR_A, CW, PWM_MAX);
-        motorOff(MOTOR_B);
+        motorGo(MOTOR_A, CW, 0);
+        motorGo(MOTOR_B, CW, PWM_MAX);
       }
       break;
 
@@ -260,8 +248,8 @@ void loop() {
       {
         digitalWrite(10, LOW);
         digitalWrite(12, LOW);
-        motorOff(MOTOR_A);
-        motorOff(MOTOR_B);
+        motorGo(MOTOR_A, CW, 0);
+        motorGo(MOTOR_B, CW, 0);
       }
       break;
 
@@ -279,8 +267,8 @@ void loop() {
       {
         digitalWrite(10, LOW);
         digitalWrite(12, LOW);
-        motorOff(MOTOR_A);
-        motorOff(MOTOR_B);
+        motorGo(MOTOR_A, CW, 0);
+        motorGo(MOTOR_B, CW, 0);
       }
       break;
   }
